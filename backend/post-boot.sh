0ee591a0691ee60137e891ab31b781022a4933a6
Cory Waddingham <cory@pinecone.io>
2025-06-17 09:47:32 -0700

    Minor adjustments to post-boot.sh to add more options for verbosity and better error handling.

--- backend/post-boot.sh
+++ backend/post-boot.sh
@@ -1,20 +1,31 @@
 #!/usr/bin/env bash
 
-set -x
+if [[ "$DEBUG" == "true" ]]; then
+    set -x
+fi
 
-cd backend
-
-python3 -m venv .venv && source .venv/bin/activate && pip install -r requirements.txt
+cd backend || echo "Error: Could not cd to backend directory, are you in the correct directory?" && exit 1
 
 if [[ -z "$PINECONE_API_KEY" ]]; then
     echo -n "Please enter your Pinecone API key: "
     read PINECONE_API_KEY
 fi
-
 export PINECONE_API_KEY=$PINECONE_API_KEY
 
-echo "Downloading checkpoints..."
-./download_ckpts.sh
+echo "Creating virtual environment..."
+if [[ ! -d ".venv" ]]; then 
+    python3 -m venv .venv 
+fi
+source .venv/bin/activate && pip install -r requirements.txt
+
+echo "Installing checkpoints..."
+CHECKPOINT_DIR="app/checkpoints"
+if [ -z "$(ls -A "$CHECKPOINT_DIR")" ]; then
+  echo "Checkpoints not found, downloading..."
+  ./download_ckpts.sh
+else
+  echo "Checkpoints already present, skipping download."
+fi
 
 echo "Starting backend server..."
 uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
